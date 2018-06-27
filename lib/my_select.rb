a920732b51b698b9a7249b037f92318300131899
Aubree Abril <aubree.abril@gmail.com>
2018-06-27 23:08:18 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ select = []
+ while i < collection.length
+   if yield(array[i])
+     select << array[i]
+   end
+   i += 1
+ end
+ select
+end
+
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
 end
