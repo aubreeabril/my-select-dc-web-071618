4c940367d7cd516218dc1f9edc92d138bcf2e98e
Aubree Abril <aubree.abril@gmail.com>
2018-06-27 23:08:22 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -10,14 +10,3 @@ def my_select(collection)
  select
 end
 
-def my_select(array)
-  i = 0
-  select = []
-  while i < array.length
-    if yield(array[i])
-      select << array[i]
-    end
-    i+=1
-  end
-  select
-end
