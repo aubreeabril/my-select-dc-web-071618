8727d303e9d63ef7e2c6892cc90170867efe34d2
Aubree Abril <aubree.abril@gmail.com>
2018-06-27 23:10:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ select = [] #selected elements will go here
+ while i < collection.length # loop through element in collection
+   if yield(collection[i])
+     select << collection[i] # move element into select
+   end
+   i += 1 # loop to the next one
+ end
+ select #return it
 end
